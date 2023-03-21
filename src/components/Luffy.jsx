3e07d9a4808a05ed5d0df9a8aeb0abe9cc2d5f61
Mathias Scriffignano <scriffignano.mathias@gmail.com>
2023-03-21 18:23:02 +0100

    1.5

--- src/components/Luffy.jsx
+++ src/components/Luffy.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+
 
 function Luffy() {
+  const [episodes,setEpisodes] = useState([]);
+
+  useEffect(()=> {
+    fetch('https://api.api-onepiece.com/episodes')
+    .then(res => res.json())
+    .then(data => setEpisodes(data))
+    .then(err => console.log(err))
+  })
+  const colors = ['bg-red-200', 'bg-blue-200', 'bg-green-200', 'bg-yellow-200'];
   return (
-    <div>Luffy</div>
+    <div className='flex flex-col justify-center'>
+      <h2 className='text-3xl text-center'>One Piece Episodes</h2>
+      <ul>
+  {episodes.map((episode, index) => (
+    <li style={{margin: '50px'}} className={colors[index % colors.length]} key={episode.episode_number}>
+      <span className='text-xl text-amber-500'>{episode.number}-{episode.title} :</span>  <br/>{episode.description}
+    </li>
+  ))}
+</ul>
+    </div>
   )
 }
 
